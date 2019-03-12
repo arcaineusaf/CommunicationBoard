2246a14c43a4168fef5534d9764fe720ccf8a17e
Joe Harter <testdrivenjoe@gmail.com>
2019-03-11 21:30:38 -0400

    Wrap to next line

--- src/icons.js
+++ src/icons.js
@@ -4,5 +4,11 @@ export default [
   { name: "peach", speech: "peach", svg: null, tags: ["fruit"] },
   { name: "apple", speech: "apple", svg: null, tags: ["fruit"] },
   { name: "pear", speech: "pear", svg: null, tags: ["fruit"] },
-  { name: "grape", speech: "grape", svg: null, tags: ["fruit"] }
+  { name: "grape", speech: "grape", svg: null, tags: ["fruit"] },
+  { name: "bread", speech: "bread", svg: null, tags: ["food"] },
+  { name: "sandwich", speech: "sandwich", svg: null, tags: ["food"] },
+  { name: "pizza", speech: "pizza", svg: null, tags: ["food"] },
+  { name: "hot dog", speech: "hot dog", svg: null, tags: ["food"] },
+  { name: "spaghetti", speech: "spaghetti", svg: null, tags: ["food"] },
+  { name: "milk", speech: "milk", svg: null, tags: ["food"] }
 ];
