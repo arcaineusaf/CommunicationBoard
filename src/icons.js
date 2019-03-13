e509919a5935f0294cdd555843f38ad3e46bf104
Joe Harter <testdrivenjoe@gmail.com>
2019-03-12 21:17:33 -0400

    Add actual svgs

--- src/icons.js
+++ src/icons.js
@@ -1,14 +1,19 @@
 export default [
-  { name: "banana", speech: "banana", svg: null, tags: ["fruit"] },
-  { name: "strawberry", speech: "strawberry", svg: null, tags: ["fruit"] },
-  { name: "peach", speech: "peach", svg: null, tags: ["fruit"] },
-  { name: "apple", speech: "apple", svg: null, tags: ["fruit"] },
-  { name: "pear", speech: "pear", svg: null, tags: ["fruit"] },
-  { name: "grape", speech: "grape", svg: null, tags: ["fruit"] },
-  { name: "bread", speech: "bread", svg: null, tags: ["food"] },
-  { name: "sandwich", speech: "sandwich", svg: null, tags: ["food"] },
-  { name: "pizza", speech: "pizza", svg: null, tags: ["food"] },
-  { name: "hot dog", speech: "hot dog", svg: null, tags: ["food"] },
-  { name: "spaghetti", speech: "spaghetti", svg: null, tags: ["food"] },
-  { name: "milk", speech: "milk", svg: null, tags: ["food"] }
+  { name: "banana", speech: "banana", svg: "banana", tags: ["fruit"] },
+  {
+    name: "strawberry",
+    speech: "strawberry",
+    svg: "strawberry",
+    tags: ["fruit"]
+  },
+  { name: "peach", speech: "peach", svg: "peach", tags: ["fruit"] },
+  { name: "apple", speech: "apple", svg: "apple", tags: ["fruit"] },
+  { name: "pear", speech: "pear", svg: "pear", tags: ["fruit"] },
+  { name: "grapes", speech: "grapes", svg: "grapes", tags: ["fruit"] },
+  { name: "bread", speech: "bread", svg: "bread", tags: ["food"] },
+  { name: "sandwich", speech: "sandwich", svg: "sandwich", tags: ["food"] },
+  { name: "pizza", speech: "pizza", svg: "pizza", tags: ["food"] },
+  { name: "hot dog", speech: "hot dog", svg: "hotdog", tags: ["food"] },
+  { name: "spaghetti", speech: "spaghetti", svg: "spaghetti", tags: ["food"] },
+  { name: "milk", speech: "milk", svg: "milk", tags: ["food"] }
 ];
